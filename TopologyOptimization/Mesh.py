e0a6971a71fd2a16a15287ff8aaa72a02f3508f6
Blake McCulligh <blake@mcculligh.com>
2026-01-15 20:26:59 -0500

    Topolagy Optimization Adding

--- TopologyOptimization/Mesh.py
+++ TopologyOptimization/Mesh.py
@@ -1,14 +1,21 @@
 import math
 
 import numpy as np
+from matplotlib import pyplot as plt
 
 from TopologyOptimization.Zone import Zone
 
 class Mesh:
-    def __init__(self, zone, maxSpassing):
+    def __init__(self, zone, maxSpassing, CollinerarTollerance):
         self.zone = zone
         self.maxSpassing = maxSpassing
         self.nodes = None
+        self.members = None
+
+        self.CollinerarTollerance = CollinerarTollerance
+
+        self.H = None
+        self.A1 = None
 
     def generateNodes(self):
         minX, minY, maxX, maxY = self.zone.getMostExtreamPoints()
@@ -63,6 +70,19 @@ class Mesh:
 
         nodesToRemove = np.where(~inside)[0]
 
+        nodesToRemove = list(nodesToRemove)
+
+        indecesToRemove = []
+        for i in range(len(nodesToRemove)):
+            for j in range(len(self.zone.nodes)):
+                n1 = self.nodes[nodesToRemove[i]]
+                n2 = self.zone.nodes[j]
+                if n1[0] == n2[0] and n1[1] == n2[1]:
+                    indecesToRemove.append(i)
+
+        for index in sorted(indecesToRemove, reverse=True):
+            del nodesToRemove[index]
+
         self.nodes = np.delete(nodeArray, nodesToRemove, axis=0)
 
     def addNodesToEadgeOfZoneWhereNeeded(self, minX, minY, numGapsX, numGapsY, dx, dy):
@@ -113,6 +133,12 @@ class Mesh:
                         point = np.array([[point[0], point[1]]])
                         self.nodes = np.append(self.nodes, point, axis =0)
 
+        # for i in range(len(self.nodes)):
+        #     for j in range(len(self.nodes)):
+        #         if i != j and self.nodes[i][0] == self.nodes[j][0] and self.nodes[i][1] == self.nodes[j][1]:
+        #             print("node Deleted")
+        #             del self.nodes[i]
+
     def doesPointExist(self, point):
 
         for node in self.nodes:
@@ -120,6 +146,138 @@ class Mesh:
                 return True
 
         return False
+
+    def print(self):
+        plt.plot(self.nodes[:,0], self.nodes[:,1], 'o')
+
+        for i in range(len(self.members)):
+             xi, xf, = self.nodes[self.members[i][0], 0], self.nodes[self.members[i][1], 0]
+             yi, yf, = self.nodes[self.members[i][0], 1], self.nodes[self.members[i][1], 1]
+             plt.plot([xi, xf], [yi, yf], color='gray')
+
+        plt.show()
+
+    def generateLevel1Members(self):
+        A = np.zeros((len(self.nodes),len(self.nodes)))
+
+        for i in range(len(self.nodes)):
+            x = self.nodes[i][0]
+            y = self.nodes[i][1]
+            closestXIndex = None
+            disToClosestX = math.inf
+            closestYIndex = None
+            disToClosestY = math.inf
+            for j in range(len(self.nodes)):
+
+                if i != j:
+                    if x == self.nodes[j][0]:
+                        dist = y - self.nodes[j][1]
+                        if dist < 0:
+                            if abs(dist) < disToClosestX:
+                                disToClosestX = dist
+                                closestXIndex = j
+
+                    elif y == self.nodes[j][1]:
+                        dist = x - self.nodes[j][0]
+                        if dist < 0:
+                            if abs(dist) < disToClosestY:
+                                disToClosestY = dist
+                                closestYIndex = j
+
+            if closestXIndex is not None:
+                A[i,closestXIndex] = 1
+            if closestYIndex is not None:
+                A[i, closestYIndex] = 1
+
+        A = A + A.T
+        n = self.getListOfUnderseportedNodes(A)
+        if len(n) != 0:
+            repet = True
+        else:
+            repet = False
+        while repet:
+
+            node = self.nodes[n[0]]
+
+            distClosest = math.inf
+            closestIndex = None
+            for i in range(len(self.nodes)):
+                dist = ((node[0]-self.nodes[i][0])**2 + (node[1]-self.nodes[i][1])**2)**0.5
+                if dist < distClosest and n[0] != i and A[n[0],i] == 0:
+                    distClosest = dist
+                    closestIndex = i
+
+            A[n[0],closestIndex] = 1
+
+            A = A + A.T
+            n = self.getListOfUnderseportedNodes(A)
+            if len(n) == 0:
+                repet = False
+
+        self.H = A
+        self.A1 = A
+        self.convertMatrixToMembers(A)
+
+    def convertMatrixToMembers(self, A):
+        self.members = []
+
+        for i in range(len(A)):
+            for j in range(len(A[i])):
+                if A[i][j] > 0 and i > j:
+                    self.members.append([i,j])
+
+    def getListOfUnderseportedNodes(self, A):
+        n = [] #TODO if corner node make only need 2 members
+        for i in range(len(A)):
+            numMembers = 0
+            for j in range(len(A[i])):
+                if A[i][j] > 0 and i != j:
+                    numMembers += 1
+            if numMembers < 3:
+                n.append(i)
+
+        return n
+
+    def generateUpperLevelMembers(self, level):
+        An = self.A1
+        Anlow = None
+        for i in range(level - 1):
+            Anlow = An
+            An = An @ self.A1
+
+        Gn = An - Anlow
+
+        #TODO remove all new members that are within a tolerance of colinarity of an already existing members
+        # figure out what is happening at the top of the mesh.
+        #self.members = []
+        # m = np.array(self.members)
+        # n = np.array(self.nodes)
+        #
+        # d = [n[m[:, 1],0] - n[m[:, 0],0], n[m[:, 1],1] - n[m[:, 0],1]]
+        # l = ((d[0]) ** 2 + (d[1]) ** 2) ** 0.5
+        #
+        # unitVectors = np.array(d)/l
+        #
+        # for i in range(len(Gn)):
+        #     for j in range(len(Gn)):
+        #         if i != j and Gn[i,j] > 0:
+        #             dnew = [n[i,0] - n[j,0], n[i,1] - n[j,1]]
+        #             lnew = ((dnew[0]) ** 2 + (dnew[1]) ** 2) ** 0.5
+        #             unitVectorNew = np.array(dnew) / lnew
+        #
+        #             unitVectorsNew = np.array([unitVectorNew] * len(unitVectors.T))
+        #
+        #             C = unitVectors * unitVectorsNew.T
+        #
+        #             #print(np.max(C))
+        #             if np.max(C) > self.CollinerarTollerance:
+        #                 Gn[i,j] = 0
+
+        self.H = self.H + Gn
+        self.H = self.H + self.H.T
+
+        self.convertMatrixToMembers(self.H)
+
 
 
 
@@ -130,5 +288,10 @@ Zone = Zone(nodes, edges)
 
 MaxSpassing = [5,5]
 
-mesh = Mesh(Zone, MaxSpassing)
+mesh = Mesh(Zone, MaxSpassing, 0.999)
 mesh.generateNodes()
+mesh.generateLevel1Members()
+# mesh.generateUpperLevelMembers(2)
+# mesh.generateUpperLevelMembers(3)
+# mesh.generateUpperLevelMembers(4)
+mesh.print()
---
+++ TopologyOptimization/testing.py
@@ -0,0 +1,53 @@
+import cvxpy as cp
+import numpy as np
+
+# Data
+volume_bound = 5
+l = np.array([5, 6, 7, 3, 5], dtype=float)
+m = len(l)
+
+d = 2 * np.sum([1, 0, 0, 0, 1])
+
+B = np.array([
+    [1, 2],
+    [2, 3],
+    [3, 4],
+    [4, 5],
+    [5, 1]
+], dtype=float)
+
+E = np.diag([5, 5, 5, 5, 5])   # IMPORTANT FIX
+
+# Variables
+w = cp.Variable()
+a = cp.Variable(m)
+
+# f must be length 2 to match B'
+f = cp.Variable(2)
+
+# Diagonal term
+D = cp.diag(cp.multiply(l, a))
+
+# Lower-right block (this is the correct order)
+K = B.T @ E @ D @ B   # (2×2)
+
+# SDP block matrix
+M = cp.bmat([
+    [cp.reshape(w, (1, 1)), cp.reshape(f, (1, 2))],
+    [cp.reshape(f, (2, 1)), K]
+])
+
+# Constraints
+constraints = [
+    M >> 0,
+    l @ a <= volume_bound,
+    a >= 0
+]
+
+# Problem
+prob = cp.Problem(cp.Minimize(w), constraints)
+prob.solve(solver=cp.SCS)
+
+print("Status:", prob.status)
+print("w:", w.value)
+print("a:", a.value)
